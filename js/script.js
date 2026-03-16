522f11c745eeb1421c4c40db19c857847e5b9959
Lera <may_be_lera@inbox.ru>
2026-03-16 12:38:18 +0300

    .

---
+++ js/script.js
@@ -0,0 +1,72 @@
+document.addEventListener('click', burgerInit);
+
+function burgerInit(e) {
+    const burgerIcon = e.target.closest('.burger-icon');
+    const burgerNavLink = e.target.closest('.nav-link');
+    // const headerButtons = document.querySelector('.header__btns')
+
+    if(!burgerIcon && !burgerNavLink) return
+    if(document.documentElement.clientWidth > 1000) return
+
+
+    if(!document.body.classList.contains('body--opened-menu')){
+        document.body.classList.add('body--opened-menu')
+    } else {
+        document.body.classList.remove('body--opened-menu')
+    }
+}
+
+
+
+
+// --------------feedback-slider
+const swiper = new Swiper('.feedback__slider', {
+    spaceBetween: 10,
+    slidesPerView: 3, 
+    // centeredSlides: true, 
+        // slidesOffsetBefore: 1,
+        // initialSlide: 0,
+    loop: true,
+});
+
+
+// --------------accordion-faq
+const accordionLists = document.querySelectorAll('.accordion-list');
+
+accordionLists.forEach(el => {
+
+  el.addEventListener('click', (e) => {
+
+    const accordionList = e.currentTarget
+    const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+    const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+    const accordionControl = e.target.closest('.accordion-list__control');
+    if (!accordionControl) return
+    e.preventDefault()
+    const accordionItem = accordionControl.parentElement;
+    const accordionContent = accordionControl.nextElementSibling;
+
+    if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+      accordionOpenedItem.classList.remove('accordion-list__item--opened');
+      accordionOpenedContent.style.maxHeight = null;
+    }
+    accordionItem.classList.toggle('accordion-list__item--opened');
+
+    if (accordionItem.classList.contains('accordion-list__item--opened')) {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+    } else {
+      accordionContent.style.maxHeight = null;
+    }
+
+    });
+
+});
+
+
+// --------------input-tel
+document.addEventListener('DOMContentLoaded', function() {
+  if (typeof Inputmask === 'function') {
+    Inputmask("+7 (999) 999-99-99").mask('input[type="tel"]');
+  }
+});
